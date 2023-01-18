097c6e1288df15f6970a7f0243ee8840782a90a0
Stensis <irenenjuguna98@gmail.com>
2023-01-19 00:34:36 +0300

    added styling to the product card

--- shopie/src/components/Products.js
+++ shopie/src/components/Products.js
@@ -12,45 +12,72 @@ function Products() {
         <div class="row row-cols-1 row-cols-md-3 g-4 mt-1 rounded ">
           <div class="col">
             <div class="card h-70 p-3 bg-opacity-10 border border-info border-start-4 rounded-end">
-             
               <div class="card-body t-center">
                 <h5 class="card-title">Card title</h5>
-                <img src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" className="card-img-top rounded border" alt="shoe"/>
+                <img
+                  src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+                  className="card-img-top rounded border"
+                  alt="shoe"
+                />
                 <p className="card-text mt-1">description of the product</p>
                 <a href="/cart" class="btn btn-primary m-1">
                   Add to cart
                 </a>
-                <button className="m-3 rounded border border-danger"  onClick={handleClick}> ❤️ {like}</button>
+                <button
+                  className="m-3 rounded border border-danger"
+                  onClick={handleClick}
+                >
+                  {" "}
+                  ❤️ {like}{" "}
+                </button>
               </div>
             </div>
           </div>
           <div class="col">
-          <div class="card h-70 p-3 bg-opacity-10 border border-info border-start-4 rounded-end">
-             
-             <div class="card-body t-center">
-               <h5 class="card-title">Card title</h5>
-               <img src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" className="card-img-top rounded border" alt="shoe"/>
-               <p className="card-text mt-1">description of the product</p>
-               <a href="/cart" class="btn btn-primary m-1">
-                 Add to cart
-               </a>
-               <button className="m-3 rounded border border-danger"  onClick={handleClick}> ❤️ {like}</button>
-             </div>
-           </div>
+            <div class="card h-70 p-3 bg-opacity-10 border border-info border-start-4 rounded-end">
+              <div class="card-body t-center">
+                <h5 class="card-title">Card title</h5>
+                <img
+                  src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+                  className="card-img-top rounded border"
+                  alt="shoe"
+                />
+                <p className="card-text mt-1">description of the product</p>
+                <a href="/cart" class="btn btn-primary m-1">
+                  Add to cart
+                </a>
+                <button
+                  className="m-3 rounded border border-danger"
+                  onClick={handleClick}
+                >
+                  {" "}
+                  ❤️ {like}{" "}
+                </button>
+              </div>
+            </div>
           </div>
           <div class="col">
-          <div class="card h-70 p-3 bg-opacity-10 border border-info border-start-4 rounded-end">
-             
-             <div class="card-body t-center">
-               <h5 class="card-title">Card title</h5>
-               <img src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" className="card-img-top rounded border" alt="shoe"/>
-               <p className="card-text mt-1">description of the product</p>
-               <a href="/cart" class="btn btn-primary m-1">
-                 Add to cart
-               </a>
-               <button className="m-3 rounded border border-danger"  onClick={handleClick}> ❤️ {like}</button>
-             </div>
-           </div>
+            <div class="card h-70 p-3 bg-opacity-10 border border-info border-start-4 rounded-end">
+              <div class="card-body t-center">
+                <h5 class="card-title">Card title</h5>
+                <img
+                  src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+                  className="card-img-top rounded border"
+                  alt="shoe"
+                />
+                <p className="card-text mt-1">description of the product</p>
+                <a href="/cart" class="btn btn-primary m-1">
+                  Add to cart
+                </a>
+                <button
+                  className="m-3 rounded border border-danger"
+                  onClick={handleClick}
+                >
+                  {" "}
+                  ❤️ {like}{" "}
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- shopie/src/components/Profile.js
+++ shopie/src/components/Profile.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const user = {
-  name: "Irene",
+  name: "Irene njuguna",
   imageUrl:
     "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
   imageSize: "70px"
@@ -10,18 +10,45 @@ const user = {
 function Profile() {
   return (
     <>
-      {/* <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={"Photo of " + user.name}
-        style={{
-          border: "1px solid blue",
-          borderRadius: "30px",
-          height: user.imageSize
-        }}
-      /> */}
-      
+      <div>
+        <img
+          className="avatar m-4"
+          src={user.imageUrl}
+          alt={"Photo of " + user.name}
+          style={{
+            border: "1px solid blue",
+            borderRadius: "40px",
+            height: user.imageSize
+          }}
+        ></img>
+        <div class="dropdown m-3">
+          <button
+            class="btn btn-secondary dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            {user.name}
+          </button>
+          <ol class="dropdown-menu">
+            <li>
+              <a class="dropdown-item" href="/cart">
+                Your Cart
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="/logout">
+                Logout
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="/profile">
+                Back ⬆️
+              </a>
+            </li>
+          </ol>
+        </div>
+      </div>
     </>
   );
 }
