3bb28e9dcb9831de03fab754c9405f97b4906c53
Stensis <irenenjuguna98@gmail.com>
2023-01-20 19:49:47 +0300

    changed /home to /

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       <Navbar />
 
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
 
         <Route path="/products" element={<Products />} />
 
